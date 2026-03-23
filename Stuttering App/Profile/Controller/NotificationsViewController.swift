01172cf75ab8fbc9b2f36538d80b4a437b568d88
Amay <amayputhiyedath@gmail.com>
2026-03-23 16:47:25 +0530

    supababse changes and reading mode animation temporary changes

---
+++ Stuttering App/Profile/Controller/NotificationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationsViewController.swift
+//  Stuttering App
+//
+//  Created by sdc - user on 23/03/26.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
